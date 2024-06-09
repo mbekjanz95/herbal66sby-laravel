70e390b232933749a557b77e91c60d706103a804
mbekjanz95 <66designandprintsby@gmail.com>
2024-06-09 08:08:13 +0700

    Penambahan footer.blade

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,16 @@
+<footer id="footer" class="mt-4 text-center">
+    <div class="mt-4" style="padding-top: 10px; color: white; font-size: 22px; opacity: 0.8;">Ikuti kami
+    </div>
+    <div id="ikon" class="mt-2 mb-2 social-icons text-center">
+        <ul>
+            <li><a target="_blank" href="https://www.instagram.com/herbal66sby/"><img width="30px" height="30px" style="filter: brightness(0) invert(1) opacity(80%); margin-top: 10px" src="instagram.svg"></a></li>
+            <li><a target="_blank" href="https://www.olx.co.id/profile/8406350"><img width="30px" height="30px" style="filter: brightness(0) invert(1) opacity(80%); margin-top: 10px" src="olx.svg"></a></li>
+        </ul>
+    </div>
+    <div class="container">
+        <p style="color: white; font-size: 16px; opacity: 0.8;">Kritik dan Saran silahkan langsung dikirimkan ke nomor Whatsapp kami : 0896-9936-4779 </p>
+        <div class="footer-line"></div>
+    </div>
+    <div class="mt-2" id="copyright-text" style="padding-bottom: 20px; color: white; font-size: 16px; opacity: 0.8;">Copyright © 2024 - Herbal 66 Surabaya
+    </div>
+</footer>
